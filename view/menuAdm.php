4850eeefea0e5d544d795e93d680e872ffb1c1e3
Caio <caiodib10@gmail.com>
2024-06-19 16:36:40 -0300

    bloqueio de rota na pagina do adm
    mudei o arquivo
    loginUsuario
    menuAdm
    logout.php

--- view/menuAdm.php
+++ view/menuAdm.php
@@ -1,12 +1,34 @@
+<?php
+include_once "../control/gerenciadorSessao.php";
+
+GerenciadorSessao::iniciarSessao(); // Inicia a sessão usando a classe GerenciadorSessao
+
+// Verifica se o usuário está autenticado
+if (!isset($_SESSION['usuario_autenticado']) || $_SESSION['usuario_autenticado'] !== true) {
+    header("Location: login.php");
+    exit();
+}
+
+// Verifica se o usuário é um administrador
+if ($_SESSION['contribuidor'] !== "administradores") {
+    header("Location: login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador</title>
+    <title>Menu Administrador</title>
 </head>
 <body>
-    <a href="gerenciarFuncionario.php"><h2>Gerenciar funcionários</h2></a>
-    <a href="gerenciarProduto.php"><h2>Gerenciar produtos</h2></a>
+    <h1>Bem-vindo, Administrador!</h1>
+    <ul>
+        <a href="gerenciarFuncionario.php">Gerenciar funcionários</a> <br>
+        <a href="gerenciarProduto.php">Gerenciar produtos</a><br>
+        <a href="../control/logout.php">Sair</a><br>
+    </ul>
 </body>
 </html>
